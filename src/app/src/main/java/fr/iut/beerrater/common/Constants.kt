de2369730edf0593da24e8319fd6571b8c0d9c3d
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-29 11:25:34 +0100

    [#8] Add also a constant file for this project

---
+++ src/app/src/main/java/fr/iut/beerrater/common/Constants.kt
@@ -0,0 +1,7 @@
+package fr.iut.beerrater.common
+
+object Constants {
+    const val API_URL = "https://api.punkapi.com/"
+    const val API_VERSION_USED = "v2"
+    const val FULL_API_URL = "$API_URL$API_VERSION_USED/"
+}
