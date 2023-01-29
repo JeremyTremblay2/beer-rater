398cef5d726eea61737f4d4e8a4da37388c7c8dd
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-29 11:20:54 +0100

    [#5] Create the beer database

---
+++ src/app/src/main/java/fr/iut/beerrater/data/data_source/database/BeerDatabase.kt
@@ -0,0 +1,26 @@
+package fr.iut.beerrater.data.data_source.database
+
+import android.app.Application
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import fr.iut.beerrater.domain.model.BeerWithReviews
+import fr.iut.beerrater.domain.model.Review
+import fr.iut.beerrater.domain.model.Volume
+import fr.iut.beerrater.data.data_source.database.converters.VolumeUnitToIntConverter
+import fr.iut.beerrater.domain.model.Beer
+
+@Database(
+    entities = [Beer::class, BeerWithReviews::class, Review:: class, Volume::class],
+    version = 1
+)
+@TypeConverters(VolumeUnitToIntConverter::class)
+abstract class BeerDatabase : RoomDatabase() {
+
+    abstract fun beerDAO(): BeerDao
+
+    companion object {
+        const val BEER_DB_FILENAME = "beers.db"
+    }
+}
