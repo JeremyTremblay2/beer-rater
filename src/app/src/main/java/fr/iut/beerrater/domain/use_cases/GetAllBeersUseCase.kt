a9afb9b41b069f88418f293e1257f499ea877be6
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-30 01:43:23 +0100

    Add a first use case

---
+++ src/app/src/main/java/fr/iut/beerrater/domain/use_cases/GetAllBeersUseCase.kt
@@ -0,0 +1,13 @@
+package fr.iut.beerrater.domain.use_cases
+
+import androidx.lifecycle.LiveData
+import fr.iut.beerrater.domain.model.Beer
+import fr.iut.beerrater.domain.repository.BeerRepository
+
+class GetAllBeersUseCase(
+    private val repository: BeerRepository
+) {
+    suspend operator fun invoke(): LiveData<List<Beer>> {
+        return repository.getAllBeers()
+    }
+}
