7053c867d56c6f71a2b37224493bf3f26a28e91c
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-30 01:42:07 +0100

    Fix wrong entity type in the database

--- src/app/src/main/java/fr/iut/beerrater/data/data_source/database/BeerDatabase.kt
+++ src/app/src/main/java/fr/iut/beerrater/data/data_source/database/BeerDatabase.kt
@@ -5,6 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import fr.iut.beerrater.data.data_source.database.converters.DateToLongConverter
 import fr.iut.beerrater.domain.model.BeerWithReviews
 import fr.iut.beerrater.domain.model.Review
 import fr.iut.beerrater.domain.model.Volume
@@ -12,10 +13,10 @@ import fr.iut.beerrater.data.data_source.database.converters.VolumeUnitToIntConv
 import fr.iut.beerrater.domain.model.Beer
 
 @Database(
-    entities = [Beer::class, BeerWithReviews::class, Review:: class, Volume::class],
+    entities = [Beer::class, Review:: class, Volume::class],
     version = 1
 )
-@TypeConverters(VolumeUnitToIntConverter::class)
+@TypeConverters(VolumeUnitToIntConverter::class, DateToLongConverter::class)
 abstract class BeerDatabase : RoomDatabase() {
 
     abstract fun beerDAO(): BeerDao
