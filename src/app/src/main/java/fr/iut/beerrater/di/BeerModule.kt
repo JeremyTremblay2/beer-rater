0d3bf59ffd2c80c546d6689a30c2f4e3b9e3c0c5
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2023-01-30 01:47:58 +0100

    Fix build.gradle with a new dependency

--- src/app/src/main/java/fr/iut/beerrater/di/BeerModule.kt
+++ src/app/src/main/java/fr/iut/beerrater/di/BeerModule.kt
@@ -1,6 +1,7 @@
 package fr.iut.beerrater.di
 
 import android.app.Application
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import com.google.gson.GsonBuilder
 import dagger.Module
@@ -12,8 +13,10 @@ import fr.iut.beerrater.data.data_source.api.BeerApi
 import fr.iut.beerrater.data.data_source.database.BeerDatabase
 import fr.iut.beerrater.data.repository.BeerRepositoryImpl
 import fr.iut.beerrater.domain.repository.BeerRepository
+import fr.iut.beerrater.domain.use_cases.GetAllBeersUseCase
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Scope
 import javax.inject.Singleton
 
 @Module
@@ -51,4 +54,8 @@ object BeerModule {
     fun provideBeerRepository(database: BeerDatabase, api: BeerApi): BeerRepository {
         return BeerRepositoryImpl(database.beerDAO(), api)
     }
+
+    @Provides
+    @Singleton
+    fun provideGetAllBeersUseCase(repository: BeerRepository) = GetAllBeersUseCase(repository)
 }
